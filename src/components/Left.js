1762a563c5f997d7ea5c016be008d1332b793b01
siddav83 <siddav83@gmail.com>
2022-12-25 19:36:07 +0000

    added target blank to links

--- src/components/Left.js
+++ src/components/Left.js
@@ -2,83 +2,93 @@ import React from "react";
 import { Link } from "@mui/material";
 
 export const Left = () => {
-    return (
-        <div className="left" style={{ zIndex: "1000" }}>
-            <Link
-                href="https://github.com/siddav83"
-                sx={{
-                    "&:hover": {
-                        transform: "translateY(-5px)",
-                    },
-                    transition: "transform 500ms",
-                    textDecoration: "none",
-                }}
-            >
-                <i
-                    style={{ color: "white" }}
-                    className="fa-brands fa-github left-icon"
-                ></i>
-            </Link>
-            <Link
-                href="https://www.instagram.com/sons_of_a_butchers/"
-                sx={{
-                    "&:hover": {
-                        transform: "translateY(-5px)",
-                    },
-                    transition: "transform 500ms",
-                    textDecoration: "none",
-                }}
-            >
-                <i
-                    style={{ color: "white" }}
-                    className="fa-brands fa-instagram left-icon"
-                ></i>
-            </Link>
-            <Link
-                href="https://www.linkedin.com/in/siddav83/"
-                sx={{
-                    "&:hover": {
-                        transform: "translateY(-5px)",
-                    },
-                    transition: "transform 500ms",
-                    textDecoration: "none",
-                }}
-            >
-                <i
-                    style={{ color: "white" }}
-                    className="fa-brands fa-linkedin left-icon"
-                ></i>
-            </Link>
-            <Link
-                href="https://twitter.com/daveydawie"
-                sx={{
-                    "&:hover": {
-                        transform: "translateY(-5px)",
-                    },
-                    transition: "transform 500ms",
-                    textDecoration: "none",
-                }}
-            >
-                <i
-                    style={{ color: "white" }}
-                    className="fa-brands fa-twitter left-icon"
-                ></i>
-            </Link>
-            <Link
-                href="https://dev.to/siddav83"
-                sx={{
-                    "&:hover": {
-                        transform: "translateY(-5px)",
-                    },
-                    transition: "transform 500ms",
-                    textDecoration: "none",
-                }}
-            >
-                <i
-                    style={{ color: "white" }}
-                    className="fa-brands fa-dev left-icon"
-                ></i>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="left" style={{ zIndex: "1000" }}>
+      <Link
+        href="https://github.com/siddav83"
+        el="noopener"
+        target="_blank"
+        sx={{
+          "&:hover": {
+            transform: "translateY(-5px)",
+          },
+          transition: "transform 500ms",
+          textDecoration: "none",
+        }}
+      >
+        <i
+          style={{ color: "white" }}
+          className="fa-brands fa-github left-icon"
+        ></i>
+      </Link>
+      <Link
+        el="noopener"
+        target="_blank"
+        href="https://www.instagram.com/sons_of_a_butchers/"
+        sx={{
+          "&:hover": {
+            transform: "translateY(-5px)",
+          },
+          transition: "transform 500ms",
+          textDecoration: "none",
+        }}
+      >
+        <i
+          style={{ color: "white" }}
+          className="fa-brands fa-instagram left-icon"
+        ></i>
+      </Link>
+      <Link
+        el="noopener"
+        target="_blank"
+        href="https://www.linkedin.com/in/siddav83/"
+        sx={{
+          "&:hover": {
+            transform: "translateY(-5px)",
+          },
+          transition: "transform 500ms",
+          textDecoration: "none",
+        }}
+      >
+        <i
+          style={{ color: "white" }}
+          className="fa-brands fa-linkedin left-icon"
+        ></i>
+      </Link>
+      <Link
+        el="noopener"
+        target="_blank"
+        href="https://twitter.com/daveydawie"
+        sx={{
+          "&:hover": {
+            transform: "translateY(-5px)",
+          },
+          transition: "transform 500ms",
+          textDecoration: "none",
+        }}
+      >
+        <i
+          style={{ color: "white" }}
+          className="fa-brands fa-twitter left-icon"
+        ></i>
+      </Link>
+      <Link
+        el="noopener"
+        target="_blank"
+        href="https://dev.to/siddav83"
+        sx={{
+          "&:hover": {
+            transform: "translateY(-5px)",
+          },
+          transition: "transform 500ms",
+          textDecoration: "none",
+        }}
+      >
+        <i
+          style={{ color: "white" }}
+          className="fa-brands fa-dev left-icon"
+        ></i>
+      </Link>
+    </div>
+  );
 };
